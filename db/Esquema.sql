326481f3014611aef76a2b4ff8c14133647a18e9
MrAllende1973 <cbautista.m@hotmail.com>
2024-06-22 06:32:57 -0400

    Implementación de Workers Concurrentes y Mejora de Logging y Manejo de Errores

--- db/Esquema.sql
+++ db/Esquema.sql
@@ -15,76 +15,76 @@ USE `GeoTrackMine`;
 -- Table `GeoTrackMine`.`Despachadores`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `GeoTrackMine`.`Despachadores` (
-  `dispatcherID` VARCHAR(255) PRIMARY KEY,
-  `name` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) UNIQUE NOT NULL
+    `dispatcherID` VARCHAR(255) PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) UNIQUE NOT NULL
 ) ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `GeoTrackMine`.`Alertas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `GeoTrackMine`.`Alertas` (
-  `alertID` VARCHAR(255) PRIMARY KEY,
-  `alertType` VARCHAR(255) NOT NULL,
-  `message` TEXT NOT NULL,
-  `timestamp` DATETIME NOT NULL,
-  `dispatcherID` VARCHAR(255),
-  FOREIGN KEY (`dispatcherID`) REFERENCES `GeoTrackMine`.`Despachadores`(`dispatcherID`) ON DELETE SET NULL
+    `alertID` VARCHAR(255) PRIMARY KEY,
+    `alertType` VARCHAR(255) NOT NULL,
+    `message` TEXT NOT NULL,
+    `timestamp` DATETIME NOT NULL,
+    `dispatcherID` VARCHAR(255),
+    FOREIGN KEY (`dispatcherID`) REFERENCES `GeoTrackMine`.`Despachadores`(`dispatcherID`) ON DELETE SET NULL
 ) ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `GeoTrackMine`.`GPSData`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `GeoTrackMine`.`GPSData` (
-  `fileID` VARCHAR(255) NOT NULL,
-  `batchID` INT NOT NULL AUTO_INCREMENT,
-  `data` JSON NOT NULL,
-  `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `originalFileName` VARCHAR(255) NOT NULL,
-  `fileDate` DATETIME NOT NULL,
-  PRIMARY KEY (`batchID`),
-  UNIQUE KEY `unique_fileID_batchID` (`fileID`, `batchID`)
+    `fileID` VARCHAR(255) NOT NULL,
+    `batchID` INT NOT NULL AUTO_INCREMENT,
+    `data` JSON NOT NULL,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `originalFileName` VARCHAR(255) NOT NULL,
+    `fileDate` DATETIME NOT NULL,
+    PRIMARY KEY (`batchID`),
+    UNIQUE KEY `unique_fileID_batchID` (`fileID`, `batchID`)
 ) ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `GeoTrackMine`.`AlertManager`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `GeoTrackMine`.`AlertManager` (
-  `managerID` INT AUTO_INCREMENT PRIMARY KEY,
-  `alertThresholds` JSON NOT NULL,
-  `dataBuffer` JSON NOT NULL,
-  `alertsGenerated` JSON NOT NULL,
-  `lastProcessedTimestamp` DATETIME NOT NULL,
-  `processingInterval` INT NOT NULL
+    `managerID` INT AUTO_INCREMENT PRIMARY KEY,
+    `alertThresholds` JSON NOT NULL,
+    `dataBuffer` JSON NOT NULL,
+    `alertsGenerated` JSON NOT NULL,
+    `lastProcessedTimestamp` DATETIME NOT NULL,
+    `processingInterval` INT NOT NULL
 ) ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `GeoTrackMine`.`HTTPLogs`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `GeoTrackMine`.`HTTPLogs` (
-  `logId` INT AUTO_INCREMENT PRIMARY KEY,
-  `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `requestMethod` VARCHAR(10),
-  `requestURL` VARCHAR(255),
-  `userAgent` VARCHAR(255),
-  `clientIP` VARCHAR(45) NULL,
-  `referer` VARCHAR(255) NULL,
-  `logLevel` VARCHAR(10) DEFAULT 'INFO',
-  `httpStatus` INT,
-  `responseTime` FLOAT,
-  `errorMessage` TEXT
+    `logId` INT AUTO_INCREMENT PRIMARY KEY,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `requestMethod` VARCHAR(10),
+    `requestURL` VARCHAR(255),
+    `userAgent` VARCHAR(255),
+    `clientIP` VARCHAR(45) NULL,
+    `referer` VARCHAR(255) NULL,
+    `logLevel` VARCHAR(10) DEFAULT 'INFO',
+    `httpStatus` INT,
+    `responseTime` FLOAT,
+    `errorMessage` TEXT
 ) ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `GeoTrackMine`.`Logs`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `GeoTrackMine`.`Logs` (
-  `logId` INT AUTO_INCREMENT PRIMARY KEY,
-  `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `level` VARCHAR(10),  -- INFO, WARNING, ERROR, DEBUG
-  `message` TEXT,
-  `component` VARCHAR(255),  -- Part of the system where the log occurs
-  `details` JSON  -- JSON to store additional structured details
+    `logId` INT AUTO_INCREMENT PRIMARY KEY,
+    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `level` VARCHAR(10),  -- INFO, WARNING, ERROR, DEBUG
+    `message` TEXT,
+    `component` VARCHAR(255),  -- Part of the system where the log occurs
+    `details` JSON  -- JSON to store additional structured details
 ) ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
